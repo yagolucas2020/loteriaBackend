70b2708ccbec2b6cf23a14a086f8c8654c94bc08
yagolucas2020 <yagolucas21.yl@gmail.com>
2022-09-05 13:32:41 -0300

    passando dados do html para java

--- src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
+++ src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
@@ -1,8 +1,11 @@
 package br.com.caelum.vraptor.controller;
 
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.model.Usuario;
 
 @Path("dashboard")
 @Controller
@@ -11,5 +14,12 @@ public class DashboardController {
 	@Get("")
 	public void dashboard() {
 		
+	}
+	
+	@Post("salvausuario")
+	public void salvaUsuario(Usuario usuario) {
+		//validar usuario
+		//salvar no banco
+		//direcionar para o dashboard
 	}
 }
